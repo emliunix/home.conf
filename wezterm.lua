fcbf06ead33eb68bb414097713cffef8a36f9ec9
emliunix <emliunix@hotmail.com>
2022-03-27 13:10:41 +0800

    iosevka font

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,19 @@
 local wezterm = require "wezterm";
 
 return {
-    
+    font = wezterm.font_with_fallback({
+        {
+            family = "Iosevka Term",
+            weight = "Regular",
+            harfbuzz_features = {
+                "calt=1",
+                "clig=1",
+                "liga=1",
+                "ss07",
+                "HSKL",
+            },
+        },
+        "Apple Color Emoji",
+    }),
+    font_size = 16,
 }
